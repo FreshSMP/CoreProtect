32930e329be7990bd3296173997c2926fa762527
R00tB33rMan <rootb33rman@gmail.com>
2025-06-01 17:03:59 -0400

    Incur (better) support for entity killing in CoreProtect

--- src/main/java/net/coreprotect/consumer/process/EntityKillProcess.java
+++ src/main/java/net/coreprotect/consumer/process/EntityKillProcess.java
@@ -10,20 +10,24 @@ import org.bukkit.entity.EntityType;
 import net.coreprotect.consumer.Consumer;
 import net.coreprotect.database.logger.EntityKillLogger;
 import net.coreprotect.utility.EntityUtils;
-import net.coreprotect.utility.Util;
 
 class EntityKillProcess {
 
     static void process(PreparedStatement preparedStmt, PreparedStatement preparedStmtEntities, int batchCount, int processId, int id, Object object, String user) {
         if (object instanceof Object[]) {
-            BlockState block = (BlockState) ((Object[]) object)[0];
-            EntityType type = (EntityType) ((Object[]) object)[1];
-            Map<Integer, List<Object>> objectLists = Consumer.consumerObjectList.get(processId);
-            if (objectLists.get(id) != null) {
-                List<Object> objectList = objectLists.get(id);
-                int entityId = EntityUtils.getEntityId(type);
-                EntityKillLogger.log(preparedStmt, preparedStmtEntities, batchCount, user, block, objectList, entityId);
-                objectLists.remove(id);
+            Object[] objects = (Object[]) object;
+
+            if (objects.length > 1 && objects[0] instanceof BlockState && objects[1] instanceof EntityType) {
+                BlockState block = (BlockState) objects[0];
+                EntityType type = (EntityType) objects[1];
+
+                Map<Integer, List<Object>> objectLists = Consumer.consumerObjectList.get(processId);
+                if (objectLists != null && objectLists.get(id) != null) {
+                    List<Object> objectList = objectLists.get(id);
+                    int entityId = EntityUtils.getEntityId(type);
+                    EntityKillLogger.log(preparedStmt, preparedStmtEntities, batchCount, user, block, objectList, entityId);
+                    objectLists.remove(id);
+                }
             }
         }
     }
