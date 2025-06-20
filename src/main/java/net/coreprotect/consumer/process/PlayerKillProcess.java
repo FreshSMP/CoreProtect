2fd3d5efe506f29f5e200261be9ae46e6a26e09a
R00tB33rMan <rootb33rman@gmail.com>
2025-06-20 12:59:57 -0400

    Likely fix an additional issue with player casting/recognition

--- src/main/java/net/coreprotect/consumer/process/PlayerKillProcess.java
+++ src/main/java/net/coreprotect/consumer/process/PlayerKillProcess.java
@@ -3,15 +3,25 @@ package net.coreprotect.consumer.process;
 import java.sql.PreparedStatement;
 
 import org.bukkit.block.BlockState;
+import org.bukkit.entity.EntityType;
 
 import net.coreprotect.database.logger.PlayerKillLogger;
 
 class PlayerKillProcess {
 
     static void process(PreparedStatement preparedStmt, int batchCount, int id, Object object, String user) {
-        if (object instanceof Object[]) {
-            BlockState block = (BlockState) ((Object[]) object)[0];
-            String player = (String) ((Object[]) object)[1];
+        if (object instanceof Object[] data && data.length >= 2) {
+            BlockState block = (BlockState) data[0];
+
+            String player;
+            if (data[1] instanceof String) {
+                player = (String) data[1];
+            } else if (data[1] instanceof EntityType) {
+                player = ((EntityType) data[1]).name();
+            } else {
+                player = "UNKNOWN";
+            }
+
             PlayerKillLogger.log(preparedStmt, batchCount, user, block, player);
         }
     }
